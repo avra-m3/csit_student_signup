c3ed2ee46050363b5ff3b69911913d5fa0bbf6e2
Avrami <avrami.hammer@gmail.com>
2018-02-28 22:51:45 +1100

    It works half the time, crashes the other half, contact me when it inevitable breaks

--- detection.py
+++ detection.py
@@ -1,55 +1,30 @@
-# import the necessary packages
-import numpy as np
+import time
 import cv2
-import argparse
 
+from information_getter import Transcriber
+
+def capture():
+    cam = cv2.VideoCapture(0)
+    cv2.namedWindow("Camera", cv2.WINDOW_NORMAL)
+    while 1:
+        status, image = cam.read()
+        # image = cv2.flip(image, 1)
+        cv2.imshow('Camera', image)
+        # print("loop")
+        key = cv2.waitKey(1)
+        if key == 32:
+            im_name = 'image_cache/temp_' + str(time.time()) + '.png'
+            cv2.imwrite(im_name, image)
+            Transcriber.singleton.do(im_name)
+        if key == 27:
+            cv2.destroyAllWindows()
+            return
+ ,
 
 def detect(image):
     # convert the image to grayscale
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    # compute the Scharr gradient magnitude representation of the images
-    # in both the x and y direction
-    gradX = cv2.Sobel(gray, ddepth=cv2.cv.CV_32F, dx=1, dy=0, ksize=-1)
-    gradY = cv2.Sobel(gray, ddepth=cv2.cv.CV_32F, dx=0, dy=1, ksize=-1)
 
-    # subtract the y-gradient from the x-gradient
-    gradient = cv2.subtract(gradX, gradY)
-    gradient = cv2.convertScaleAbs(gradient)
-
-    # blur and threshold the image
-    blurred = cv2.blur(gradient, (9, 9))
-    (_, thresh) = cv2.threshold(blurred, 225, 255, cv2.THRESH_BINARY)
-
-    # construct a closing kernel and apply it to the thresholded image
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (21, 7))
-    closed = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
-
-    # perform a series of erosions and dilations
-    closed = cv2.erode(closed, None, iterations=4)
-    closed = cv2.dilate(closed, None, iterations=4)
-
-    # find the contours in the thresholded image
-    (cnts, _) = cv2.findContours(closed.copy(), cv2.RETR_EXTERNAL,
-                                 cv2.CHAIN_APPROX_SIMPLE)
-
-    # if no contours were found, return None
-    if len(cnts) == 0:
-        return None
-
-    # otherwise, sort the contours by area and compute the rotated
-    # bounding box of the largest contour
-    c = sorted(cnts, key=cv2.contourArea, reverse=True)[0]
-    rect = cv2.minAreaRect(c)
-    box = np.int0(cv2.cv.BoxPoints(rect))
-
-    # return the bounding box of the barcode
-    return box
-
-
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True,
-                help="C:\\Users\\Avrami\\Pictures\\Camera Roll\\WIN_20180225_11_34_05_Pro.jpg")
-args = vars(ap.parse_args())
-
-print(detect(args))
+Transcriber('output.csv')
+capture()
--- information_getter.py
+++ information_getter.py
@@ -1,22 +1,150 @@
 import io
+import json
 import os
-import google.cloud.vision as vision_api
+
+# import google as google
+# import google.cloud.vision as vision_api
+import time
+
+# auth = google.discovery
+from base64 import b64encode
+
+import regex as regex
+import requests
 
 
 class Transcriber:
+    singleton = None
+
     def __init__(self, target: str):
-        self.v_client = vision_api.ImageAnnotatorClient()
+        if Transcriber.singleton is not None:
+            raise Exception("We are fucked")
+        Transcriber.singleton = self
+        # self.v_client = vision_api.ImageAnnotatorClient()
+
         self.output_csv = open(target, "w")
+        self.output_csv.write("snumber,name,email,time\n")
 
-    def do(self, content: str):
-        image = vision_api.types.Image(content=content)
-        image_content = self.v_client.label_detection(image=image)
-        for line in image_content:
-            print(line.description)
-            print(line.__dict__)
+    def interp(self, content: json):
+        snumber = None
+        firstname = {
+            'value': None,
+            'x': -1,
+            'y': -1,
+        }
+        lastname = {
+            'value': None,
+            'x': -1,
+            'y': -1,
+        }
+        full_string = []
+        for annotation in content["responses"][0]["textAnnotations"]:
+            if "locale" in annotation:
+                continue
+            text = annotation["description"]
+            print(text)
+            is_snumber = StudentCard.id.match(text) is not None
+            is_name = StudentCard.first_name.match(text) is not None
+            if text == "Expiry" or text == "Date":
+                continue
+            if is_snumber:
+                snumber = text
+            if is_name:
+                firstname['value'] = text
+                firstname['x'] = annotation['boundingPoly']["vertices"][0]["x"]
+                firstname['y'] = annotation['boundingPoly']["vertices"][0]["y"]
+        for annotation in content["responses"][0]["textAnnotations"]:
+            if "locale" in annotation:
+                full_string = annotation["description"].split("\n")
+            text = annotation["description"]
+            print(text)
+            topleft_x = annotation['boundingPoly']["vertices"][0]["x"]
+            topleft_y = annotation['boundingPoly']["vertices"][0]["y"]
+            if StudentCard.last_name.match(text) is None:
+                print(text + " invalid match")
+                continue
+            if topleft_x not in range(firstname['x'] - 10, firstname['x'] + 10):
+                print(text + " not in range")
+                continue
+            if abs(lastname['y'] - firstname['y']) < abs(topleft_y - firstname['y']):
+                print(text + " not closest match")
+                continue
+            lastname['value'] = text
+            lastname['x'] = topleft_x
+            lastname['y'] = topleft_y
+        print(snumber)
+        print(firstname)
+        print(lastname)
+        print(full_string)
+        return snumber, firstname['value'], lastname['value']
+
+    def do(self, path: str):
+        # content = ""
+        with open(path, "rb") as file:
+            content = file.read()
+        response = GCloudOCR.push(content)
+        if response.status_code != 200:
+            print("Google Down? or the internet is disconnected... choose one")
+            print(response.status_code)
+            print(response.content)
+            return
+        image_desc = None
+        print(response.content)
+        data = response.json()
+        snumber, firstname, lastname = self.interp(data)
+        if snumber is None or firstname is None or lastname is None:
+            print("Failure: Null value")
+            return
+        self.insert_record(snumber, firstname + " " + lastname)
+
+    def get_time_str(self):
+        now = time.localtime()
+        return "%.4d/%.2d/%.2d %.2d:%.2d" % (now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min)
+
+    def insert_record(self, snumber, name):
+        self.output_csv.write("%s,%s,%s,%s\n" % (snumber, name, snumber + '@rmit.edu.au', self.get_time_str()))
+        self.output_csv.flush()
 
 
-single_transcriber = Transcriber('output.csv')
-with open('WIN_20180225_11_34_05_Pro.jpg', 'r+') as image_file:
-    image_str = image_file.read()
-single_transcriber.do(image_str)
+class GCloudOCR:
+    ENDPOINT = 'https://vision.googleapis.com/v1/images:annotate'
+    AUTH = os.getenv('OCR_AUTH_KEY')
+    HEADERS = {'Content-Type': 'application/json'}
+
+    @staticmethod
+    def format_image(image: str):
+        return {
+            'image': {'content': b64encode(image).decode()},
+            "features": [
+                {
+                    "type": "TEXT_DETECTION"
+                }
+            ],
+            "imageContext": {
+                "languageHints": [
+                    "en"
+                ]
+            }
+        }
+
+    @staticmethod
+    def format_data(image: str):
+        return json.dumps(
+            {
+                'requests': [GCloudOCR.format_image(image)]
+            }
+        )
+
+    @staticmethod
+    def push(image: str) -> requests.api:
+        return requests.post(GCloudOCR.ENDPOINT,
+                             data=GCloudOCR.format_data(image),
+                             params={'key': GCloudOCR.AUTH},
+                             headers=GCloudOCR.HEADERS
+                             )
+
+
+class StudentCard:
+    id = regex.compile('^(\d{7})$')
+    first_name = regex.compile('^((?!Expiry)[A-Z][a-z]+)$')
+    last_name = regex.compile('^([A-Z\-]+)$')
